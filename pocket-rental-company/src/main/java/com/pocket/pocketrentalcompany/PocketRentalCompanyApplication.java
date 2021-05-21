7c7505aedefa845c42623d6358df86667e55e735
ParrySMS <yh@szu.me>
2021-05-21 23:54:42 +0800

    TICKET-002 init maven springboot with dependency

---
+++ pocket-rental-company/src/main/java/com/pocket/pocketrentalcompany/PocketRentalCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.pocket.pocketrentalcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketRentalCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketRentalCompanyApplication.class, args);
+	}
+
+}
---
+++ pocket-rental-company/src/test/java/com/pocket/pocketrentalcompany/PocketRentalCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket.pocketrentalcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketRentalCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
