4632cd3792020fa4244100f215466114de67f7c9
ParrySMS <yh@szu.me>
2021-05-22 13:24:16 +0800

    TICKET-003 fix sql V1

--- storage/db-sql-server/src/main/resources/db/migration/V1.00__create_table_client.sql
+++ storage/db-sql-server/src/main/resources/db/migration/V1.00__create_table_client.sql
@@ -1,20 +1,20 @@
-USE [Demo]
+USE [demo]
 GO
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Demo].[pocket_client]') AND type in (N'U'))
-DROP TABLE [Demo].[pocket_client]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[demo].[pocket_client]') AND type in (N'U'))
+DROP TABLE [demo].[pocket_client]
 GO
 
-CREATE TABLE [Demo].[pocket_client](
+CREATE TABLE [pocket_client](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](126) NOT NULL,
 	[contact] [varchar](16) NOT NULL,
 	[citizen_id] [varchar](32) NOT NULL,
 	[create_on] [datetime] NOT NULL,
 	[last_modified] [datetime] NOT NULL
-) ON [Demo]
+)
 GO
 
-ALTER TABLE [Demo].[pocket_client]
-	ADD CONSTRAINT [pk_pocket_client] PRIMARY KEY CLUSTERED ([id] ASC) ON [Demo]
+ALTER TABLE [pocket_client]
+	ADD CONSTRAINT [pk_pocket_client] PRIMARY KEY CLUSTERED ([id] ASC)
 GO
